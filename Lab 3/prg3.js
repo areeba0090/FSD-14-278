fc0c0a87c60bcfd81adddb06be578579c087b701
Areeba Alam <areeba.25b01010090@abes.ac.in>
2026-09-03 13:57:18 +0530

    lab 3

--- Lab 3/prg3.js
+++ Lab 3/prg3.js
@@ -1,26 +1,30 @@
 import http from "http";
 
-const server = http.createServer((req,res) => {
-    res.writeHead
-    if(req.url=="/")
-        res.end("<h1>Home Page</h1>")
-    else if(req.url="/about")
-        res.end("<h1>About Us Page</h1>")
-    else if(req.url="/product") {
-        res.end(`<h1>Mobile Phone</h1>
-                <h2>Price: 25000</h2>
-                <p>Discount:5%</p>
-                <a href="#">Buy Now</a>
-                `);
-        }
-    else {
-        res.studioCode = 404;
-        res.end(`
-            <h1>404 not found</h1>
-            <p>Page not found</p>
-            <a href="/">Home</a>
-            `);
-    }
-});
+const server = http.createServer((req, res) => {
 
-server.listen(4444,()=> console.log("Server is running"));
+    if(req.url == '/')
+        res.end('<h1> Home Page </h1>')
+    else if(req.url == "/about")
+        res.end('<h2> About us Page </h2>')
+    else if(req.url == "/product")
+        res.end(`<h1> Mobile Phone </h1>
+                    <h2> Price: 25000 </h2>
+                    <p> Discount: 5% </p>
+                    <a href = '#'> Buy Now </a>
+                `);
+    else{
+        res.statusCode = 404;
+        res.end(`
+                <h1> 404, Not Found </h1>
+                <p> Page not found </p>
+                <a href = '/'> Go to Home Page </a>  
+            `)
+    }
+
+
+}) ;
+
+server.listen(1519 , ()=>{
+    console.log("server is running") ;
+}
+) ;
--- Lab 3/readme.md
+++ Lab 3/readme.md
@@ -4,3 +4,60 @@ HTTP:Hyper Text transfer protocol
 HTML:Hyper Text Markup Language
 CSS:Cascading Style Sheet
 npm:Non package management
+
+### Status Codes
+
+200-OK
+201-Created
+400-Bad Request
+401-Unauthorised
+403-Forbidden
+404-Not Found
+
+# API
+
+Any API can be of 4 types (CRUD)
+
+Post - Create
+Get - Read
+Put/ Patch - Update
+Delete - Delete
+API can be responsed by server with status code and JSON data
+
+API generally starts with ex:-
+/api/v1/products
+/api/v2/products/2169
+/api/...
+by default the browser can check only GET request; to check other 3 request types like (POST/ PUT/ DELETE) we require frontend or third party API tester like postman, thunderclient, echoapi
+
+## Content type
+
+text/html -> For HTML live server
+text/json -> For JSON file Type
+text/plain -> For plain text File
+
+## Server can send data
+
+1.
+2. HTML
+3. JSON data
+4. Plain text
+5. CSS
+6. JS File
+
+## Server can set Header to send Data
+
+1. res.writeHeader()
+2. res.setHeader()
+
+## Server can set status code
+
+1. res.statusCode()
+2. res.writeHeader()
+
+## request methods
+
+1. get
+2. post
+3. put/patch
+4. delete
